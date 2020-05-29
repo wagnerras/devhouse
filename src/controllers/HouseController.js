67dd9afc1b3c6d2e379c6dda386cf378205b338c
unknown <wagnerras@gmail.com>
2020-05-29 12:54:44 -0300

    houses

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,9 +1,48 @@
+import House from '../models/House';
+
 class HouseController {
 
+    async index(req, res){
+        const { status } = req.query;
+        const houses = await House.find({status});
+
+        return res.json(houses);
+    }
+
     async store(req, res){
-        console.log(req.body);
-        console.log(req.file);
-        return res.json({ok: true});
+        const {filename} = req.file;
+        const {description, price, location, status} = req.body;
+        const {user_id} = req.headers;
+
+        const house = await House.create({
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status
+        })
+
+        return res.json(house);
+    }
+
+
+    async update(req, res){
+        const {filename} = req.file;
+        const {house_id} = req.params;
+        const {description, price, location, status} = req.body;
+        const {user_id} = req.headers;
+
+        const houses = await House.updateOne({_id: house_id}, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status
+        })
+
+        return res.json(houses);
     }
    
 }
--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,10 @@ const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
 routes.post('/houses', upload.single('thumbnail') ,HouseController.store);
+routes.get('/houses', HouseController.index);
+routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 
 //module.exports = routes;
 export default routes;
