f5ebeebce76e8b7c0eae4a8a28a8a0fa2f965746
unknown <wagnerras@gmail.com>
2020-05-30 15:53:38 -0300

    devhouse

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -9,21 +9,31 @@ destroy: deletar uma sessao
  */
 
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController {
 
-   async store(req,res){
-        const {email} = req.body;
+    async store(req, res) {
+
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        })
+
+        const { email } = req.body;
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({error: 'Falha na validação.'});
+        }
 
         //let user = await User.create({email});
-        let user = await User.findOne({email}); //verificando se esse usuario ja existe
-        if(!user){
-            user = await User.create({email}); // cria se não existir
+        let user = await User.findOne({ email }); //verificando se esse usuario ja existe
+        if (!user) {
+            user = await User.create({ email }); // cria se não existir
         }
 
         return res.json(user);
     }
-    
+
 }
 
 export default new SessionController();
